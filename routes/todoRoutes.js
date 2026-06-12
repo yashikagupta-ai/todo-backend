ee13339fff7a95b195deb4de427e0a156c92d05d
yashikagupta-ai <yashikagupta561@gmail.com>
2026-06-12 14:45:38 +0530

    Update todoRoutes.js

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,6 +1,9 @@
 import express from "express";
-import * as todoController from "../controllers/todoController.js"
-const router= express.Router();
+import * as todoController from "../controllers/todoController.js";
+import protect from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+router.use(protect);
 router.post("/", todoController.createTodo);
 router.get("/", todoController.getTodo);
 router.delete("/:id", todoController.deleteTodo);
